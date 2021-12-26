f2135cdc74190a553bcc7c8afc2f7809c921d8e3
Kilari Teja <kilariteja9@gmail.com>
2021-12-26 19:18:18 +0530

    Add /api/top endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-console.log('Hello')
+import { PrismaClient } from '@prisma/client'
+import { Logger } from 'tslog'
+import Fastify from 'fastify'
+
+const log = new Logger({
+  name: 'server',
+})
+
+// Database client.
+const prisma = new PrismaClient()
+
+// Fastify app.
+const fastify = Fastify({
+  logger: true,
+})
+
+/*
+  Returns a list of latest posts with the content on those pages.
+*/
+fastify.get('/api/top', {
+  schema: {
+    querystring: {
+      page: {
+        type: 'number',
+        default: 1,
+      },
+      page_size: {
+        type: 'number',
+        default: 30,
+      },
+    },
+  },
+  handler: async (req, res) => {
+    const query = req.query as {
+      page: number
+      page_size: number
+    }
+
+    const pageSize = Math.min(
+      query.page_size ?? 30,
+      30,
+    )
+
+    const items = await prisma.post.findMany({
+      where: {
+        visible: true,
+      },
+      orderBy: {
+        rank: 'desc',
+      },
+      include: {
+        Content: true,
+      },
+      take: pageSize,
+      skip: ((query.page ?? 1) - 1) * pageSize,
+    })
+
+    return res.send(items)
+  },
+})
+
+/*
+  Start the server.
+*/
+fastify.listen(3000, (error, address) => {
+  if (error) {
+    return log.trace(error)
+  }
+
+  log.info('Started server at', address)
+})
